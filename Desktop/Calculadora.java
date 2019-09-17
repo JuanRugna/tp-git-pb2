9ced83290ac2c59d0740d7afa2b36043e549266a
JuanRugna <juanrugna@hotmail.com>
2019-09-17 20:17:16 -0300

    Test

--- Desktop/Calculadora.java
+++ Desktop/Calculadora.java
@@ -1,28 +1,46 @@
+package git;
+
 import java.util.Scanner;
-public class MiCalculadora {
- public static void main(String[] args) {
- Integer a;
- Integer b;
- Integer opcion;
- Integer resultado;
+
+public class Calculadora {
+	public static void main(String[] args) {
+		Integer a;
+		Integer b;
+		Integer opcion;
+		Integer resultado;
+		Scanner teclado = new Scanner (System.in);
  do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 para
-restar /n 3 para multiplicar /n 4 para dividir");
- opcion = teclado.next();
- }while(opcion < 1 && opcion > 4)
- System.out.println("Ingrese el primer numero");
- a = teclado.next();
+	 	System.out.println("Ingrese su operaciÃ³n /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+	 	opcion = teclado.nextInt();
+ 	}while(opcion < 1 && opcion > 4);
+ 			System.out.println("Ingrese el primer numero");
+ a = teclado.nextInt();
  System.out.println("Ingrese el segundo numero");
- a = teclado.next();
- if(opcion.equals(1)){
- resultado = a + b;
- }else if(opcion.equals(2)){
- resultado = a - b;
- }else if(opcion.equals(3)){
- resultado = a * b;
- }else{
- resultado = a / b;
- }
- System.out.println("El resultado es " + resultado.toString());
-}
+ b = teclado.nextInt();
+ 	
+ 	switch (opcion) {
+	case 1:
+		resultado = a + b;
+		System.out.println("El resultado es " +resultado);
+		break;
+
+	case 2:
+		resultado = a - b;
+		System.out.println("El resultado es " +resultado);
+		break;
+	case 3: 
+		resultado = a * b;
+		System.out.println("El resultado es " +resultado);
+		break;
+	case 4:
+		resultado = a / b;
+		System.out.println("El resultado es " +resultado);
+		break;
+	default:
+		break;
+	}
+ 	
+
+ 	
+	}
 }
