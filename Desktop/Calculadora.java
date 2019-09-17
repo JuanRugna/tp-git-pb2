ae59b3389285b41ed6c983506688c6d8d39c4ffe
JuanRugna <juanrugna@hotmail.com>
2019-09-17 20:18:53 -0300

    Test

--- Desktop/Calculadora.java
+++ Desktop/Calculadora.java
@@ -10,7 +10,7 @@ public class Calculadora {
 		Integer resultado;
 		Scanner teclado = new Scanner (System.in);
  do{
-	 	System.out.println("Ingrese su operaciÃ³n /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+	 	System.out.println("Ingrese su operaciÃ³n /n 2 para sumar /n 1 para restar /n 4 para multiplicar /n 3 para dividir");
 	 	opcion = teclado.nextInt();
  	}while(opcion < 1 && opcion > 4);
  			System.out.println("Ingrese el primer numero");
@@ -19,20 +19,20 @@ public class Calculadora {
  b = teclado.nextInt();
  	
  	switch (opcion) {
-	case 1:
+	case 2:
 		resultado = a + b;
 		System.out.println("El resultado es " +resultado);
 		break;
 
-	case 2:
+	case 1:
 		resultado = a - b;
 		System.out.println("El resultado es " +resultado);
 		break;
-	case 3: 
+	case 4: 
 		resultado = a * b;
 		System.out.println("El resultado es " +resultado);
 		break;
-	case 4:
+	case 3:
 		resultado = a / b;
 		System.out.println("El resultado es " +resultado);
 		break;
